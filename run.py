a10b8158436696fa4d0b9f5b1944fdfbb3446f32
Jasmeen Saprah <j.sapraa@gmail.com>
2024-06-22 17:04:45 +0000

    Try catch function for incorrect customer response

--- run.py
+++ run.py
@@ -19,23 +19,39 @@ def get_random_word():
 
 def get_user_response(word):
     """
-    Receive customer response
+    Set the turns to 0 and increase the count if the user makes a wrong guess
+    Receive user response
     """
     turns = 0
+    guess = []
 
-    print(word)
+    characters = [c for c in word]
+    print(characters)
     print("Which letter do you think is the the word I am thinking of?")
 
-    while turns < 6:
+    while turns < 5:
         user_response = input()
         print(f"You guessed {user_response}.")
-        
-        if(user_response in word):
+        guess.append(user_response)
+        print(f"This is the guess{guess}")
+
+        try:
+            if(user_response != 1):
+                raise ValueError("Exact one value required")
+            elif(user_resonse in guess):
+                raise ValueError(f"You already guessed these letters {guess}.")
+        except ValueError as e:
+            print("Please provide a different input")
+            correct_value = False
+
+        if(user_response in characters):
             print("Well done. Continue guessing..")
             
         else:
             print("Oops try again")
             turns = turns + 1
+            print_hangman(turns)
+            print ("_",end="")
 
         print(turns)
 
@@ -46,5 +62,16 @@ def main():
     welcome()
     secret_word = get_random_word()
     get_user_response(secret_word)
+
+def print_hangman(turns):
+    """
+    Print the hangman images on each wrong guess
+    """
+
+    if(turns == 6):
+        print("----------")
+        print("|    0")
+        print("|   \|/")
+        print("|   / \ ")
 
 main()
