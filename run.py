1b3bcf1472642115ec8551016e87e88a1a6a9bd3
Jasmeen Saprah <j.sapraa@gmail.com>
2024-06-24 17:22:29 +0000

    Add clearconsole function and clear code with flake8

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import os
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -13,13 +14,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_words')
 
-#Testing data import from spreadsheet
-"""
-def importing_words():
-    data = SHEET.worksheet('words')
-    words_data = data.get_all_values()
-    print(words_data)
-"""
 
 def welcome():
     """
@@ -29,16 +23,25 @@ def welcome():
     name = input()
     print(f"I have a word on my mind {name}. Can you guess it?")
 
+
 def get_random_word():
     """
     Get a random word from the given list
     """
-    random_words = ["avenue", "bookworm", "cycle", "duplex", "eardrop", "fuchsia", "galaxy", "hyphen", "injury", "jackpot", "kiosk", "luxury", "matrix", "nowadays", "oxygen", "pneumonia", "quartz", "rhubarb", "scratch", "transplant", "unknown", "vodka", "wizard", "youthful", "zodiac"]
-    data = SHEET.worksheet('words')
-    #random_words = data.get_all_values()
+    random_words = [
+        "avenue", "bookworm", "cycle", "duplex",
+        "eardrop", "fuchsia", "galaxy", "hyphen",
+        "injury", "jackpot", "kiosk", "luxury",
+        "matrix", "nowadays", "oxygen", "pneumonia",
+        "quartz", "rhubarb", "scratch", "transplant",
+        "unknown", "vodka", "wizard", "youthful", "zodiac"]
+
+    # data = SHEET.worksheet('words')
+    # random_words = data.get_all_values()
     secret_word = random.choice(random_words)
 
-    return(secret_word)
+    return (secret_word)
+
 
 def validate_user_response(word):
     """
@@ -49,24 +52,24 @@ def validate_user_response(word):
     guess = []
     characters = [c for c in word]
     hidden_value = ["_ "] * len(characters)
-    hide = "_ " *len(characters)
+    hide = "_ " * len(characters)
     print(f"The word I am thinking of is: {hide}")
-    
+
     print(characters)
-    
+
     print("Which letter do you think is the the word I am thinking of?")
 
     while failed_attempt < 5:
         user_response = input()
         print(f"You guessed {user_response}.")
 
-        if(user_response in guess):
+        if (user_response in guess):
             print(f"You already guessed these letters {guess}. Try again!")
 
-        elif(user_response not in "a,b,c,d,e,f,g,hi,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"):
-            print("Letter not in the alphabet or multiple values used. Try again!")
+        elif (user_response.isalpha() == False):
+            print("Letter not in the alphabet. Try again!")
 
-        elif(user_response in characters):
+        elif (user_response in characters):
             print("Well done. Continue guessing..")
             guess.append(user_response)
 
@@ -82,6 +85,17 @@ def validate_user_response(word):
 
         print(f"Failed attempts: {failed_attempt}")
 
+
+def clearConsole():
+    """
+    Clear the console
+    """
+    command = "clear"
+    if os.name in ("nt", "dos"):
+        command = "cls"
+    os.system(command)
+
+
 def main():
     """
     Run all program functions
@@ -89,5 +103,6 @@ def main():
     welcome()
     secret_word = get_random_word()
     validate_user_response(secret_word)
+
 
 main()
